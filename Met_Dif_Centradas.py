b7fcd0e9fe57c26261fec3a502779bca0dbb6a07
francisco espinola <franespinola_92@hotmail.com>
2023-08-18 23:11:09 -0300

    agregue error porcentual

--- Met_Dif_Centradas.py
+++ Met_Dif_Centradas.py
@@ -11,6 +11,9 @@ class DiferenciasCentradas:
     def diferencia_centrada_4_puntos(self, funcion, h, x):
         return (funcion.subs('x', x - 2*h) - 8*funcion.subs('x', x - h) + 8*funcion.subs('x', x + h) - funcion.subs('x', x + 2*h)) / (12 * h)
         ##con subs sustituimos a la funcion por las operaciones establecidas por 'x' y 'h'
+
+    def derivada_metodo_directo(self,funcion,x):
+        return funcion.diff('x').subs('x', x)
 def main():
     try:
         funcion_expresion = input("Ingresa la función a evaluar: ")
@@ -18,15 +21,17 @@ def main():
         
         h = float(input("Ingresa el valor de h: "))
         x = float(input("Ingresa el valor de x: "))
+        derivada_exacta = funcion.diff('x').subs('x', x)
         
         diferencias = DiferenciasCentradas()
         
         opcion = 0
         
-        while opcion not in [1, 2]:
+        while opcion not in [1, 2, 3]:
             print("Elige una opción:")
             print("1. Diferencia centrada en 2 puntos")
             print("2. Diferencia centrada en 4 puntos")
+            print("3. Metodo directo")
             
             try:
                 opcion = int(input())
@@ -37,8 +42,14 @@ def main():
             resultado = diferencias.diferencia_centrada_2_puntos(funcion, h, x)
         elif opcion == 2:
             resultado = diferencias.diferencia_centrada_4_puntos(funcion, h, x)
+        elif opcion == 3:
+            resultado = diferencias.derivada_metodo_directo(funcion, x)    
         
         print("Resultado:", resultado)
+        
+        ##calculo de error porcentual
+        error_porcentual = abs((derivada_exacta- resultado) / derivada_exacta) * 100
+        print("Error porcentual:", error_porcentual)
     except ValueError:
         print("Ingrese valores numéricos válidos")
 
